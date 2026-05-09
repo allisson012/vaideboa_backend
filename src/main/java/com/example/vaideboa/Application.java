7a653d20228fa451a9560bc845ff50c370e3280a
allisson012 <allissoncastilho@gmail.com>
2026-05-09 15:27:46 -0300

    Desabilitando o sheduling durante o desenvolvimento

--- src/main/java/com/example/vaideboa/Application.java
+++ src/main/java/com/example/vaideboa/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling -> deixei comentado para não ficar rodando schudeled durante desenvolvimento
 public class Application {
 
 	public static void main(String[] args) {
